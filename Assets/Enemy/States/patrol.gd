87a8c5c8d8b4d22fd323b378d901536570232a17
Selim <selassin@gmail.com>
2025-04-27 16:38:41 +0300

    Enemy patrol improvement

--- Assets/Enemy/States/patrol.gd
+++ Assets/Enemy/States/patrol.gd
@@ -8,18 +8,19 @@ func enter(previous_state_path: String, data := {}) -> void:
 	enemy.velocity.x = 0.0
 	$PatrolTimer.start()
 	patrol_dir = -enemy.facing
-	enemy.call_deferred("update_animation", "run")
 func exit() -> void:
 	$PatrolTimer.stop()
 func update(delta : float) -> void:
 	#Decide if we are going to leave the player alone or chase
-	if enemy.chase_target:
+	if enemy.chase_target && !enemy.target_obstructed():
 		if enemy.chase_target.unconscious:
 			finished.emit("leave_player")
-		elif !enemy.target_obstructed():
+		else:
 			finished.emit("chase")
 	elif !enemy.move(enemy.patrol_move_speed, patrol_dir):
 		finished.emit("idle")
+	else:
+		enemy.update_animation("run")
 
 func _on_patrol_timer_timeout() -> void:
 	finished.emit("idle")
