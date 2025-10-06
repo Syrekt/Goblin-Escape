4fca2628380a8764d296d8b613eee23a3e43d387
Selim <selassin@gmail.com>
2025-10-06 09:43:10 +0300

    Adding portal, need to separate Light from Torch

--- Map/Fast Travel/portal.gd
+++ Map/Fast Travel/portal.gd
@@ -10,7 +10,26 @@ func _ready() -> void:
 	activate.connect(_on_activate)
 	use.connect(_on_use)
 
+func update(player: Player) -> void:
+	active = !inert
+	if inert:
+		return
+
+	if Input.is_action_just_pressed("interact"):
+		# Do something
+		pass
+
 func _on_activate() -> void:
-	pass
+	inert = false
+	save()
 func _on_use() -> void:
 	pass
+
+func save() -> void:
+	Ge.save_node(self,{
+		"inert": false,
+	})
+func load(data: Dictionary) -> void:
+	inert = data.get(inert, true)
+	if !inert:
+		$Sprite.play("active")
